322d0aadb7c134c8122c37902980cd862ea51de7
Derrick Ho <derrickho328@gmail.com>
2015-03-28 11:52:14 -0700

    - Added UIButton Programmatically

--- MyButton/ViewController.swift
+++ MyButton/ViewController.swift
@@ -8,8 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainView : UIView {
+    var coolButton:(UIButton)!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setUpViews()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setUpViews()
+    }
+    
+    func setUpViews() {
+        self.backgroundColor = UIColor.redColor()
+//        var xWidth = 200.0
+//        var yHeight = 50.0
+//        var xPos = Float(self.frame.size.width) / 2.0
+//        xPos -= Float(xWidth) / 2.0
+//        var yPos = Float(self.frame.size.height) / 2.0
+//        yPos -= Float(yHeight) / 2.0
+//        
+//        self.coolButton = UIButton(frame:
+//            CGRect(x: Int(xPos),
+//                y: Int(yPos),
+//                width: Int(xWidth),
+//                height: Int(yHeight)))
+        self.coolButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        self.coolButton.center = self.center
+        self.addSubview(self.coolButton)
+        self.coolButton.backgroundColor = UIColor.blueColor()
+    }
+}
 
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        self.view = MainView(frame: UIScreen.mainScreen().bounds)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
